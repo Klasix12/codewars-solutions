e95c865793b010925ddd483714689f33b8cd6a69
Artem Oleshev <sunlightpattern@yandex.ru>
2024-05-28 12:52:26 +0300

    persist

--- src/kata6/Persist.java
+++ src/kata6/Persist.java
@@ -1,13 +1,6 @@
 package kata6;
 
 class Persist {
-    public static void main(String[] args) {
-        System.out.println(persistence(39)); // 3
-        System.out.println(persistence(4)); // 0
-        System.out.println(persistence(25)); // 2
-        System.out.println(persistence(999)); // 4
-    }
-
     public static int persistence(long n) {
         if (n < 10) {
             return 0;
