9a122239b6c31d798fcd8af463007eaf69d3d0a4
Artem Oleshev <sunlightpattern@yandex.ru>
2024-05-31 20:12:33 +0300

    solution: Adding Big Numbers

---
+++ src/kata4/AddingBigNumbers.java
@@ -0,0 +1,34 @@
+package kata4;
+
+public class Kata {
+    public static String add(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int maxLength = Math.max(a.length(), b.length());
+        b = "0".repeat(maxLength - b.length()) + b;
+        a = "0".repeat(maxLength - a.length()) + a;
+
+        boolean addOne = false;
+        for (int i = b.length() - 1; i >= 0; i--) {
+            int vle = Character.getNumericValue(a.charAt(i)) + Character.getNumericValue(b.charAt(i));
+            if (addOne) {
+                vle += 1;
+                addOne = false;
+            }
+            if (vle >= 10) {
+                addOne = true;
+                sb.append(vle % 10);
+            } else {
+                sb.append(vle);
+            }
+        }
+        if (addOne) {
+            sb.append("1");
+        }
+
+        while (sb.charAt(sb.length() - 1) == '0') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        return sb.reverse().toString();
+    }
+}
